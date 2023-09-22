073f4716bf077115ac5888bd1778cf667e8fd29d
liammelkersson <liammelkersson@live.se>
2023-09-22 15:18:52 +0200

    initiated tailwind and added images

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// ! tailwindcss v3.3.3 | MIT License | https://tailwindcss.com
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  jit: true, // Enable JIT mode
+};
