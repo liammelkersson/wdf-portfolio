6816a8222add49be8b027114c50030aea88ac3ba
liammelkersson <liammelkersson@live.se>
2023-09-23 18:18:55 +0200

    README + some notes for future features

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+
+const port = 3000;
+const app = express(); // creates the Express application
+
+// defines handlebars engine
+app.engine("handlebars", engine());
+// defines the view engine to be handlebars
+app.set("view engine", "handlebars");
+// defines the views directory
+app.set("views", "./views");
+
+// define static directory "public" to access css/, img/ and vid/
+app.use(express.static("public"));
+
+// MODEL (DATA)
+const humans = [
+  { id: "0", name: "Jerome" },
+  { id: "1", name: "Mira" },
+  { id: "2", name: "Linus" },
+  { id: "3", name: "Susanne" },
+  { id: "4", name: "Jasmin" },
+];
+
+// CONTROLLER (THE BOSS)
+// defines route "/"
+app.get("/", function (request, response) {
+  response.render("home.hbs");
+});
+
+// defines route "/humans"
+app.get("/humans", function (request, response) {
+  const model = { listHumans: humans }; // defines the model
+  // in the next line, you should send the abovedefined
+  // model to the page and not an empty object {}...
+  response.render("humans.hbs", model);
+});
+
+// defines route "/humans/1"
+app.get("/humans/1", function (request, response) {
+  const model = humans[1]; // defines the model
+  // in the next line, you should send the abovedefined
+  // model to the page and not an empty object {}...
+  response.render("human.hbs", model);
+});
+
+// defines the final default route 404 NOT FOUND
+app.use(function (req, res) {
+  res.status(404).render("404.hbs");
+});
+
+// runs the app and listens to the port
+app.listen(port, () => {
+  console.log(`Express server listening on http://localhost:${port}/`);
+});
